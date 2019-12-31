a3a9a3fb10579c2cc54bc2d29206f0bf1d1f6586
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 11:36:27 -0600

    Rewriting as Laminas Project package

--- src/Assertion/AssertionInterface.php
+++ src/Assertion/AssertionInterface.php
@@ -1,22 +1,20 @@
 <?php
+
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2012 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
- * @package   Zend_Permissions
+ * @see       https://github.com/laminas/laminas-permissions-acl for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-permissions-acl/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-permissions-acl/blob/master/LICENSE.md New BSD License
  */
 
-namespace Zend\Permissions\Acl\Assertion;
+namespace Laminas\Permissions\Acl\Assertion;
 
-use Zend\Permissions\Acl\Acl;
-use Zend\Permissions\Acl\Resource\ResourceInterface;
-use Zend\Permissions\Acl\Role\RoleInterface;
+use Laminas\Permissions\Acl\Acl;
+use Laminas\Permissions\Acl\Resource\ResourceInterface;
+use Laminas\Permissions\Acl\Role\RoleInterface;
 
 /**
- * @category   Zend
- * @package    Zend_Permissions
+ * @category   Laminas
+ * @package    Laminas_Permissions
  * @subpackage Acl
  */
 interface AssertionInterface
