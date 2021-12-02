924eda3e3b701473dbbec669917d8ce5f4015903
Witold Wasiczko <snapshotpl@users.noreply.github.com>
2021-12-02 16:05:17 +0100

    Revert "Remove file headers"

--- test/TestAsset/StandardUseCase/User.php
+++ test/TestAsset/StandardUseCase/User.php
@@ -1,5 +1,11 @@
 <?php
 
+/**
+ * @see       https://github.com/laminas/laminas-permissions-acl for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-permissions-acl/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-permissions-acl/blob/master/LICENSE.md New BSD License
+ */
+
 namespace LaminasTest\Permissions\Acl\TestAsset\StandardUseCase;
 
 use Laminas\Permissions\Acl\Role\RoleInterface;
