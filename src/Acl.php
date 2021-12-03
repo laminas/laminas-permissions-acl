laminas/laminas-permissions-acl

commit 624567fe376a70e0bfb5aa8217d5afa13b9d6e61

Rewriting as Laminas Project package

src/Acl.php
@@ -1,13 +1,12 @@
 <?php
+
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2015 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @see       https://github.com/laminas/laminas-permissions-acl for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-permissions-acl/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-permissions-acl/blob/master/LICENSE.md New BSD License
  */
 
-namespace Zend\Permissions\Acl;
+namespace Laminas\Permissions\Acl;
 
 class Acl implements AclInterface
 {
@@ -104,7 +103,7 @@ class Acl implements AclInterface
             $role = new Role\GenericRole($role);
         } elseif (! $role instanceof Role\RoleInterface) {
             throw new Exception\InvalidArgumentException(
-                'addRole() expects $role to be of type Zend\Permissions\Acl\Role\RoleInterface'
+                'addRole() expects $role to be of type Laminas\Permissions\Acl\Role\RoleInterface'
             );
         }
 
@@ -232,7 +231,7 @@ class Acl implements AclInterface
             $resource = new Resource\GenericResource($resource);
         } elseif (! $resource instanceof Resource\ResourceInterface) {
             throw new Exception\InvalidArgumentException(
-                'addResource() expects $resource to be of type Zend\Permissions\Acl\Resource\ResourceInterface'
+                'addResource() expects $resource to be of type Laminas\Permissions\Acl\Resource\ResourceInterface'
             );
         }
 
@@ -697,7 +696,7 @@ class Acl implements AclInterface
      *
      * If either $role or $resource is null, then the query applies to all Roles or all Resources,
      * respectively. Both may be null to query whether the ACL has a "blacklist" rule
-     * (allow everything to all). By default, Zend\Permissions\Acl creates a "whitelist" rule (deny
+     * (allow everything to all). By default, Laminas\Permissions\Acl creates a "whitelist" rule (deny
      * everything to all), and this method would return false unless this default has
      * been overridden (i.e., by executing $acl->allow()).
      *
@@ -948,14 +947,14 @@ class Acl implements AclInterface
     ) {
         if (null === $privilege) {
             /**
-             * @see Zend\Permissions\Acl\Exception
+             * @see Laminas\Permissions\Acl\Exception
              */
             throw new Exception\RuntimeException('$privilege parameter may not be null');
         }
 
         if (null === $dfs) {
             /**
-             * @see Zend\Permissions\Acl\Exception
+             * @see Laminas\Permissions\Acl\Exception
              */
             throw new Exception\RuntimeException('$dfs parameter may not be null');
         }

test/AclTest.php
@@ -1,21 +1,20 @@
 <?php
+
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2015 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @see       https://github.com/laminas/laminas-permissions-acl for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-permissions-acl/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-permissions-acl/blob/master/LICENSE.md New BSD License
  */
 
-namespace ZendTest\Permissions\Acl;
+namespace LaminasTest\Permissions\Acl;
 
+use Laminas\Permissions\Acl;
+use Laminas\Permissions\Acl\Exception\ExceptionInterface;
+use Laminas\Permissions\Acl\Exception\InvalidArgumentException;
+use Laminas\Permissions\Acl\Resource;
+use Laminas\Permissions\Acl\Role;
 use PHPUnit\Framework\TestCase;
 use stdClass;
-use Zend\Permissions\Acl;
-use Zend\Permissions\Acl\Exception\ExceptionInterface;
-use Zend\Permissions\Acl\Exception\InvalidArgumentException;
-use Zend\Permissions\Acl\Resource;
-use Zend\Permissions\Acl\Role;
 
 class AclTest extends TestCase
 {
@@ -122,7 +121,7 @@ class AclTest extends TestCase
     public function testRoleRegistryAddNotRole()
     {
         $this->expectException(InvalidArgumentException::class);
-        $this->expectExceptionMessage('addRole() expects $role to be of type Zend\Permissions\Acl\Role');
+        $this->expectExceptionMessage('addRole() expects $role to be of type Laminas\Permissions\Acl\Role');
         $this->acl->addRole(new \stdClass, 'guest');
     }
 
@@ -138,7 +137,7 @@ class AclTest extends TestCase
         try {
             $this->acl->inheritsRole('nonexistent', $roleGuest);
             $this->fail(
-                'Expected Zend\Permissions\Acl\Role\Exception not thrown upon specifying a non-existent child Role'
+                'Expected Laminas\Permissions\Acl\Role\Exception not thrown upon specifying a non-existent child Role'
             );
         } catch (Acl\Exception\InvalidArgumentException $e) {
             $this->assertContains('not found', $e->getMessage());
@@ -146,7 +145,7 @@ class AclTest extends TestCase
         try {
             $this->acl->inheritsRole($roleGuest, 'nonexistent');
             $this->fail(
-                'Expected Zend\Permissions\Acl\Role\Exception not thrown upon specifying a non-existent child Role'
+                'Expected Laminas\Permissions\Acl\Role\Exception not thrown upon specifying a non-existent child Role'
             );
         } catch (Acl\Exception\InvalidArgumentException $e) {
             $this->assertContains('not found', $e->getMessage());
@@ -313,7 +312,7 @@ class AclTest extends TestCase
     /**
      * Ensures that basic addition and retrieval of a single Resource works
      *
-     * @group ZF-1167
+     * @group Laminas-1167
      */
     public function testResourceAddAndGetOneWithAddResourceMethod()
     {
@@ -386,7 +385,7 @@ class AclTest extends TestCase
     public function testResourceRegistryAddNotResource()
     {
         $this->expectException(InvalidArgumentException::class);
-        $this->expectExceptionMessage('addResource() expects $resource to be of type Zend\Permissions\Acl\Resource');
+        $this->expectExceptionMessage('addResource() expects $resource to be of type Laminas\Permissions\Acl\Resource');
         $this->acl->addResource(new stdClass);
     }
 
@@ -402,7 +401,7 @@ class AclTest extends TestCase
         try {
             $this->acl->inheritsResource('nonexistent', $resourceArea);
             $this->fail(
-                'Expected Zend\Permissions\Acl\Exception\ExceptionInterface not'
+                'Expected Laminas\Permissions\Acl\Exception\ExceptionInterface not'
                 . ' thrown upon specifying a non-existent child Resource'
             );
         } catch (Acl\Exception\ExceptionInterface $e) {
@@ -411,7 +410,7 @@ class AclTest extends TestCase
         try {
             $this->acl->inheritsResource($resourceArea, 'nonexistent');
             $this->fail(
-                'Expected Zend\Permissions\Acl\Exception\ExceptionInterface not thrown '
+                'Expected Laminas\Permissions\Acl\Exception\ExceptionInterface not thrown '
                 . 'upon specifying a non-existent parent Resource'
             );
         } catch (Acl\Exception\ExceptionInterface $e) {
@@ -488,7 +487,7 @@ class AclTest extends TestCase
         try {
             $this->acl->isAllowed('nonexistent');
             $this->fail(
-                'Expected Zend\Permissions\Acl\Role\Exception\ExceptionInterface not thrown upon non-existent Role'
+                'Expected Laminas\Permissions\Acl\Role\Exception\ExceptionInterface not thrown upon non-existent Role'
             );
         } catch (Acl\Exception\InvalidArgumentException $e) {
             $this->assertContains('not found', $e->getMessage());
@@ -496,7 +495,7 @@ class AclTest extends TestCase
         try {
             $this->acl->isAllowed(null, 'nonexistent');
             $this->fail(
-                'Expected Zend\Permissions\Acl\Exception\ExceptionInterface not thrown upon non-existent Resource'
+                'Expected Laminas\Permissions\Acl\Exception\ExceptionInterface not thrown upon non-existent Resource'
             );
         } catch (Acl\Exception\InvalidArgumentException $e) {
             $this->assertContains('not found', $e->getMessage());
@@ -504,7 +503,7 @@ class AclTest extends TestCase
     }
 
     /**
-     * Ensures that by default, Zend_Acl denies access to everything by all
+     * Ensures that by default, Laminas_Acl denies access to everything by all
      *
      * @return void
      */
@@ -539,7 +538,7 @@ class AclTest extends TestCase
     }
 
     /**
-     * Ensures that by default, Zend_Acl denies access to a privilege on anything by all
+     * Ensures that by default, Laminas_Acl denies access to a privilege on anything by all
      *
      * @return void
      */
@@ -620,7 +619,7 @@ class AclTest extends TestCase
     }
 
     /**
-     * Ensures that by default, Zend_Acl denies access to everything for a particular Role
+     * Ensures that by default, Laminas_Acl denies access to everything for a particular Role
      *
      * @return void
      */
@@ -649,7 +648,7 @@ class AclTest extends TestCase
     }
 
     /**
-     * Ensures that by default, Zend_Acl denies access to a privilege on anything for a particular Role
+     * Ensures that by default, Laminas_Acl denies access to a privilege on anything for a particular Role
      *
      * @return void
      */
@@ -878,7 +877,7 @@ class AclTest extends TestCase
         try {
             $this->acl->isAllowed('guest');
             $this->fail(
-                'Expected Zend\Permissions\Acl\Role\Exception not thrown upon isAllowed() on non-existent Role'
+                'Expected Laminas\Permissions\Acl\Role\Exception not thrown upon isAllowed() on non-existent Role'
             );
         } catch (Acl\Exception\InvalidArgumentException $e) {
             $this->assertContains('not found', $e->getMessage());
@@ -903,7 +902,7 @@ class AclTest extends TestCase
         try {
             $this->acl->isAllowed('guest');
             $this->fail(
-                'Expected Zend\Permissions\Acl\Role\Exception not thrown upon isAllowed() on non-existent Role'
+                'Expected Laminas\Permissions\Acl\Role\Exception not thrown upon isAllowed() on non-existent Role'
             );
         } catch (Acl\Exception\InvalidArgumentException $e) {
             $this->assertContains('not found', $e->getMessage());
@@ -927,7 +926,7 @@ class AclTest extends TestCase
         try {
             $this->acl->isAllowed(null, 'area');
             $this->fail(
-                'Expected Zend\Permissions\Acl\Exception not thrown upon isAllowed() on non-existent Resource'
+                'Expected Laminas\Permissions\Acl\Exception not thrown upon isAllowed() on non-existent Resource'
             );
         } catch (Acl\Exception\ExceptionInterface $e) {
             $this->assertContains('not found', $e->getMessage());
@@ -951,7 +950,7 @@ class AclTest extends TestCase
         try {
             $this->acl->isAllowed(null, 'area');
             $this->fail(
-                'Expected Zend\Permissions\Acl\Exception\ExceptionInterface not thrown upon '
+                'Expected Laminas\Permissions\Acl\Exception\ExceptionInterface not thrown upon '
                 . 'isAllowed() on non-existent Resource'
             );
         } catch (Acl\Exception\ExceptionInterface $e) {
@@ -1133,7 +1132,7 @@ class AclTest extends TestCase
      * Ensures that the $onlyParents argument to inheritsRole() works
      *
      * @return void
-     * @group  ZF-2502
+     * @group  Laminas-2502
      */
     public function testRoleInheritanceSupportsCheckingOnlyParents()
     {
@@ -1145,14 +1144,14 @@ class AclTest extends TestCase
     }
 
     /**
-     * Ensures that the solution for ZF-2234 works as expected
+     * Ensures that the solution for Laminas-2234 works as expected
      *
      * @return void
-     * @group  ZF-2234
+     * @group  Laminas-2234
      */
     public function testAclInternalDFSMethodsBehaveProperly()
     {
-        $acl = new TestAsset\ExtendedAclZF2234();
+        $acl = new TestAsset\ExtendedAclLaminas2234();
 
         $someResource = new Resource\GenericResource('someResource');
         $someRole     = new Role\GenericRole('someRole');
@@ -1165,28 +1164,28 @@ class AclTest extends TestCase
 
         try {
             $acl->exroleDFSVisitAllPrivileges($someRole, $someResource, $nullReference);
-            $this->fail('Expected Zend\Permissions\Acl\Exception not thrown');
+            $this->fail('Expected Laminas\Permissions\Acl\Exception not thrown');
         } catch (Acl\Exception\ExceptionInterface $e) {
             $this->assertEquals('$dfs parameter may not be null', $e->getMessage());
         }
 
         try {
             $acl->exroleDFSOnePrivilege($someRole, $someResource, null);
-            $this->fail('Expected Zend\Permissions\Acl\Exception not thrown');
+            $this->fail('Expected Laminas\Permissions\Acl\Exception not thrown');
         } catch (Acl\Exception\ExceptionInterface $e) {
             $this->assertEquals('$privilege parameter may not be null', $e->getMessage());
         }
 
         try {
             $acl->exroleDFSVisitOnePrivilege($someRole, $someResource, null);
-            $this->fail('Expected Zend\Permissions\Acl\Exception not thrown');
+            $this->fail('Expected Laminas\Permissions\Acl\Exception not thrown');
         } catch (Acl\Exception\ExceptionInterface $e) {
             $this->assertEquals('$privilege parameter may not be null', $e->getMessage());
         }
 
         try {
             $acl->exroleDFSVisitOnePrivilege($someRole, $someResource, 'somePrivilege', $nullReference);
-            $this->fail('Expected Zend\Permissions\Acl\Exception not thrown');
+            $this->fail('Expected Laminas\Permissions\Acl\Exception not thrown');
         } catch (Acl\Exception\ExceptionInterface $e) {
             $this->assertEquals('$dfs parameter may not be null', $e->getMessage());
         }
@@ -1194,7 +1193,7 @@ class AclTest extends TestCase
 
 
     /**
-     * @group ZF-1721
+     * @group Laminas-1721
      */
     public function testAclAssertionsGetProperRoleWhenInheritenceIsUsed()
     {
@@ -1204,7 +1203,7 @@ class AclTest extends TestCase
         $blogPost = new Resource\GenericResource('blogPost');
 
         /**
-         * @var ZendTest\Permissions\Acl\StandardUseCase\UserIsBlogPostOwnerAssertion
+         * @var LaminasTest\Permissions\Acl\StandardUseCase\UserIsBlogPostOwnerAssertion
          */
         $assertion = $acl->customAssertion;
 
@@ -1215,7 +1214,7 @@ class AclTest extends TestCase
 
     /**
      *
-     * @group ZF-1722
+     * @group Laminas-1722
      */
     public function testAclAssertionsGetOriginalIsAllowedObjects()
     {
@@ -1227,7 +1226,7 @@ class AclTest extends TestCase
         $this->assertTrue($acl->isAllowed($user, $blogPost, 'view'));
 
         /**
-         * @var ZendTest\Permissions\Acl\StandardUseCase\UserIsBlogPostOwnerAssertion
+         * @var LaminasTest\Permissions\Acl\StandardUseCase\UserIsBlogPostOwnerAssertion
          */
         $assertion = $acl->customAssertion;
 
@@ -1263,7 +1262,7 @@ class AclTest extends TestCase
      * Confirm that deleting a role after allowing access to all roles
      * raise undefined index error
      *
-     * @group ZF-5700
+     * @group Laminas-5700
      */
     public function testRemovingRoleAfterItWasAllowedAccessToAllResourcesGivesError()
     {
@@ -1283,10 +1282,10 @@ class AclTest extends TestCase
     }
 
     /**
-     * @group ZF-8039
+     * @group Laminas-8039
      *
      * Meant to test for the (in)existence of this notice:
-     * "Notice: Undefined index: allPrivileges in lib/Zend/Acl.php on line 682"
+     * "Notice: Undefined index: allPrivileges in lib/Laminas/Acl.php on line 682"
      */
     public function testMethodRemoveAllowDoesNotThrowNotice()
     {
@@ -1312,11 +1311,11 @@ class AclTest extends TestCase
     }
 
     /**
-     * @group ZF-7973
+     * @group Laminas-7973
      */
     public function testAclPassesPrivilegeToAssertClass()
     {
-        $assertion = new TestAsset\AssertionZF7973();
+        $assertion = new TestAsset\AssertionLaminas7973();
 
         $acl = new Acl\Acl();
         $acl->addRole('role');
@@ -1328,7 +1327,7 @@ class AclTest extends TestCase
     }
 
     /**
-     * @group ZF-8468
+     * @group Laminas-8468
      */
     public function testgetRoles()
     {
@@ -1345,7 +1344,7 @@ class AclTest extends TestCase
     }
 
     /**
-     * @group ZF-8468
+     * @group Laminas-8468
      */
     public function testgetResources()
     {
@@ -1359,7 +1358,7 @@ class AclTest extends TestCase
     }
 
     /**
-     * @group ZF-9643
+     * @group Laminas-9643
      */
     public function testRemoveAllowWithNullResourceAppliesToAllResources()
     {
@@ -1386,7 +1385,7 @@ class AclTest extends TestCase
     }
 
     /**
-     * @group ZF-9643
+     * @group Laminas-9643
      */
     public function testRemoveDenyWithNullResourceAppliesToAllResources()
     {
@@ -1415,7 +1414,7 @@ class AclTest extends TestCase
     }
 
     /**
-     * @group ZF2-3454
+     * @group Laminas-3454
      */
     public function testAclResourcePermissionsAreInheritedWithMultilevelResourcesAndDenyPolicy()
     {

test/TestAsset/OwnershipUseCase/Acl.php
@@ -1,14 +1,15 @@
 <?php
+
 /**
- * @see       https://github.com/zendframework/zend-permissions-acl for the canonical source repository
- * @copyright Copyright (c) 2018 Zend Technologies USA Inc. (https://www.zend.com)
- * @license   https://github.com/zendframework/zend-permissions-acl/blob/master/LICENSE.md New BSD License
+ * @see       https://github.com/laminas/laminas-permissions-acl for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-permissions-acl/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-permissions-acl/blob/master/LICENSE.md New BSD License
  */
 
-namespace ZendTest\Permissions\Acl\TestAsset\OwnershipUseCase;
+namespace LaminasTest\Permissions\Acl\TestAsset\OwnershipUseCase;
 
-use Zend\Permissions\Acl\Acl as BaseAcl;
-use Zend\Permissions\Acl\Assertion\OwnershipAssertion;
+use Laminas\Permissions\Acl\Acl as BaseAcl;
+use Laminas\Permissions\Acl\Assertion\OwnershipAssertion;
 
 class Acl extends BaseAcl
 {

commit 99191a60af3426da8416a65148d211de88823e3c

Cumulate all allow rules with assertions
This fixes the regression introduced in 624567fe376a70e0bfb5aa8217d5afa13b9d6e61 (2.7.2),
where rules with assertions were no longer cumulated, but instead the process was
aborted on first failed assertion and the privilege was incorrectly denied.

That regression directly contradicted that official documentation that states:

> a failed assertion results in the rule not applying, and other
> rules would be used to determine whether access is allowed or denied.

That regression prevented to fallback on more generic rules. And it also made it very brittle
to work with role inheritance since the order of inheritance declaration became critically important,
while adding/removing privileges anywhere in the inheritance chain could incorrectly have side effect
for other roles in an unexpected way.

So this patch is essentially a revert of the problematic commit. And the use-case at the origin of the
regression is now solved, and tested, differently, with proper usage of (pre-)existing behavior, as it
should have been from the beginning.

So instead of the incorrect:

```php
$acl = new Acl();

$assertAllow = new TestAsset\ChildBooleanAssertion(true);
$assertDeny  = new TestAsset\ChildBooleanAssertion(false);

$acl->addRole('staff');
$acl->addResource('base');
$acl->allow('staff', 'base', 'update', $assertAllow);

$acl->addResource('user', 'base');
$acl->allow('staff', 'user', 'update', $assertDeny);
```

It should be:

```php
$acl = new Acl();
$assertAllow = new MockAssertion(true);

$acl->addRole('staff');
$acl->addResource('base');
$acl->allow('staff', 'base', 'update', $assertAllow);

$acl->addResource('user', 'base');
$acl->deny('staff', 'user', 'update', $assertAllow);
```

Fixes #12

Signed-off-by: Adrien Crivelli <adrien.crivelli@gmail.com>

src/Acl.php
@@ -1018,11 +1018,24 @@ class Acl implements AclInterface
 
         // Was a custom assertion supplied? Use it to retrieve the rule type.
         if ($rule['assert']) {
-            return $this->getRuleTypeFromAssertion($rule['assert'], $rule['type'], $role, $resource);
+            $assertion = $rule['assert'];
+            $assertionValue = $assertion->assert(
+                $this,
+                ($this->isAllowedRole instanceof Role\RoleInterface) ? $this->isAllowedRole : $role,
+                ($this->isAllowedResource instanceof Resource\ResourceInterface) ? $this->isAllowedResource : $resource,
+                $this->isAllowedPrivilege
+            );
         }
 
-        // Return the type supplied with the rule.
-        return $rule['type'];
+        if (null === $rule['assert'] || $assertionValue) {
+            return $rule['type'];
+        } elseif (null !== $resource || null !== $role || null !== $privilege) {
+            return;
+        } elseif (self::TYPE_ALLOW === $rule['type']) {
+            return self::TYPE_DENY;
+        }
+
+        return self::TYPE_ALLOW;
     }
 
     /**
@@ -1097,32 +1110,5 @@ class Acl implements AclInterface
     public function getResources()
     {
         return array_keys($this->resources);
-    }
-
-    /**
-     * Run the assertion to determine what rule type is selected
-     *
-     * Runs the assertion. When the assertion returns true, return the rule type
-     * as defined; otherwise, return its inversion.
-     *
-     * @param string $ruleType
-     * @return string
-     */
-    private function getRuleTypeFromAssertion(
-        Assertion\AssertionInterface $assertion,
-        $ruleType,
-        Role\RoleInterface $role = null,
-        Resource\ResourceInterface $resource = null
-    ) {
-        if ($assertion->assert(
-            $this,
-            $this->isAllowedRole instanceof Role\RoleInterface ? $this->isAllowedRole : $role,
-            $this->isAllowedResource instanceof Resource\ResourceInterface ? $this->isAllowedResource : $resource,
-            $this->isAllowedPrivilege
-        )) {
-            return $ruleType;
-        }
-
-        return $ruleType === self::TYPE_ALLOW ? self::TYPE_DENY : self::TYPE_ALLOW;
     }
 }

test/AclTest.php
@@ -7,6 +7,7 @@ use Laminas\Permissions\Acl\Exception\ExceptionInterface;
 use Laminas\Permissions\Acl\Exception\InvalidArgumentException;
 use Laminas\Permissions\Acl\Resource;
 use Laminas\Permissions\Acl\Role;
+use LaminasTest\Permissions\Acl\TestAsset\MockAssertion;
 use PHPUnit\Framework\TestCase;
 use stdClass;
 
@@ -513,7 +514,7 @@ class AclTest extends TestCase
      */
     public function testDefaultAssert()
     {
-        $this->acl->deny(null, null, null, new TestAsset\MockAssertion(false));
+        $this->acl->deny(null, null, null, new MockAssertion(false));
         $this->assertTrue($this->acl->isAllowed());
         $this->assertTrue($this->acl->isAllowed(null, null, 'somePrivilege'));
     }
@@ -605,10 +606,10 @@ class AclTest extends TestCase
      */
     public function testPrivilegeAssert()
     {
-        $this->acl->allow(null, null, 'somePrivilege', new TestAsset\MockAssertion(true));
+        $this->acl->allow(null, null, 'somePrivilege', new MockAssertion(true));
         $this->assertTrue($this->acl->isAllowed(null, null, 'somePrivilege'));
 
-        $this->acl->allow(null, null, 'somePrivilege', new TestAsset\MockAssertion(false));
+        $this->acl->allow(null, null, 'somePrivilege', new MockAssertion(false));
         $this->assertFalse($this->acl->isAllowed(null, null, 'somePrivilege'));
     }
 
@@ -734,10 +735,10 @@ class AclTest extends TestCase
         $roleGuest = new Role\GenericRole('guest');
         $this->acl
             ->addRole($roleGuest)
-            ->allow($roleGuest, null, 'somePrivilege', new TestAsset\MockAssertion(true));
+            ->allow($roleGuest, null, 'somePrivilege', new MockAssertion(true));
         $this->assertTrue($this->acl->isAllowed($roleGuest, null, 'somePrivilege'));
 
-        $this->acl->allow($roleGuest, null, 'somePrivilege', new TestAsset\MockAssertion(false));
+        $this->acl->allow($roleGuest, null, 'somePrivilege', new MockAssertion(false));
         $this->assertFalse($this->acl->isAllowed($roleGuest, null, 'somePrivilege'));
     }
 
@@ -760,7 +761,7 @@ class AclTest extends TestCase
      */
     public function testRemoveDefaultDenyAssert()
     {
-        $this->acl->deny(null, null, null, new TestAsset\MockAssertion(false));
+        $this->acl->deny(null, null, null, new MockAssertion(false));
         $this->assertTrue($this->acl->isAllowed());
         $this->acl->removeDeny();
         $this->assertFalse($this->acl->isAllowed());
@@ -1185,7 +1186,6 @@ class AclTest extends TestCase
         }
     }
 
-
     /**
      * @group Laminas-1721
      */
@@ -1333,7 +1333,7 @@ class AclTest extends TestCase
         $this->acl->addRole(new Role\GenericRole('editor'), 'staff');
         $this->acl->addRole(new Role\GenericRole('administrator'));
 
-        $expected = ['guest', 'staff','editor','administrator'];
+        $expected = ['guest', 'staff', 'editor','administrator'];
         $this->assertEquals($expected, $this->acl->getRoles());
     }
 
@@ -1460,18 +1460,50 @@ class AclTest extends TestCase
     /**
      * @see https://github.com/laminas/laminas-permissions-acl/issues/2
      */
-    public function testWhenAssertionReturnsFalseTheInversionOfItsTypeShouldBeUsed()
+    public function testCanDenyAnInheritedRuleThatAllows()
     {
-        $assertAllow = new TestAsset\ChildBooleanAssertion(true);
-        $assertDeny  = new TestAsset\ChildBooleanAssertion(false);
+        $assertAllow = new MockAssertion(true);
 
         $this->acl->addRole('staff');
         $this->acl->addResource('base');
         $this->acl->allow('staff', 'base', 'update', $assertAllow);
 
         $this->acl->addResource('user', 'base');
-        $this->acl->allow('staff', 'user', 'update', $assertDeny);
+        $this->acl->deny('staff', 'user', 'update', $assertAllow);
 
         $this->assertFalse($this->acl->isAllowed('staff', 'user', 'update'));
+    }
+
+    /**
+     * @see https://github.com/laminas/laminas-permissions-acl/issues/12
+     */
+    public function testCanFallbackOnGenericAllowedRuleWhenSpecificAllowRuleIsRejectedWithFailingAssertion()
+    {
+        $this->acl->addRole('user');
+        $this->acl->addResource('asset');
+        $this->acl->allow('user', 'asset', 'read', new MockAssertion(false));
+        $this->acl->allow('user');
+
+        $this->assertTrue($this->acl->isAllowed('user', 'asset', 'read'));
+    }
+
+    /**
+     * @see https://github.com/laminas/laminas-permissions-acl/issues/12
+     */
+    public function testCanInheritRolesInAnyOrderAndStillCumulateAllAllowedRulesWithSuccessfulAssertions()
+    {
+        $this->acl->addRole('user-allow');
+        $this->acl->addRole('user-deny');
+        $this->acl->addRole('super-user-1', ['user-deny', 'user-allow']);
+        $this->acl->addRole('super-user-2', ['user-allow', 'user-deny']);
+        $this->acl->addResource('asset');
+
+        $this->acl->allow('user-allow', 'asset', 'read', new MockAssertion(true));
+        $this->acl->allow('user-deny', 'asset', 'read', new MockAssertion(false));
+
+        $this->assertTrue($this->acl->isAllowed('user-allow', 'asset', 'read'));
+        $this->assertFalse($this->acl->isAllowed('user-deny', 'asset', 'read'));
+        $this->assertTrue($this->acl->isAllowed('super-user-1', 'asset', 'read'));
+        $this->assertTrue($this->acl->isAllowed('super-user-2', 'asset', 'read'));
     }
 }

test/TestAsset/ChildBooleanAssertion.php
@@ -1,27 +1,0 @@
-<?php
-
-namespace LaminasTest\Permissions\Acl\TestAsset;
-
-use Laminas\Permissions\Acl\Acl;
-use Laminas\Permissions\Acl\Assertion\AssertionInterface;
-use Laminas\Permissions\Acl\Resource\ResourceInterface;
-use Laminas\Permissions\Acl\Role\RoleInterface;
-
-/**
- * @see https://github.com/laminas/laminas-permissions-acl/issues/2
- */
-class ChildBooleanAssertion implements AssertionInterface
-{
-    /** @var bool */
-    private $value;
-
-    public function __construct($value)
-    {
-        $this->value = (bool) $value;
-    }
-
-    public function assert(Acl $acl, RoleInterface $role = null, ResourceInterface $resource = null, $privilege = null)
-    {
-        return $this->value;
-    }
-}